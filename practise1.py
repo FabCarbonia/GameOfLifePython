d8210d3b7927f5332210b79963ac1672ab394bdb
FapCarbonator <50168510+FapCarbonator@users.noreply.github.com>
2019-05-23 15:44:11 +0200

    Updated practise1 (class game).
    Still not working properly

--- practise1.py
+++ practise1.py
@@ -19,11 +19,14 @@ colors.append((0, 128, 128)) #blue
 
 class GameOfLife:
     def __init__(self):
-        # Starting pygame
-        pygame.init()
-        pygame.display.set_caption("Game of Life - Created by Fabio Melis")  # Gives a title to the window
-        self.screen = pygame.display.set_mode(GRID_SIZE)  # Create the window with the GRID_SIZE.
 
+
+
+        # Two lists, one for the current generation, and one for the next generation, so you can have iterations.
+        self.current_generation = [[COLOR_DEAD for y in range(Y_CELLS)] for x in range(X_CELLS)]
+        self.next_generation = [[COLOR_DEAD for y in range(Y_CELLS)] for x in range(X_CELLS)]
+        self.next_iteration = False
+        self.ended = False
 
 
         #main window
@@ -51,25 +54,21 @@ class GameOfLife:
         elif system == "Linux":
             os.environ['SDL_VIDEODRIVER'] = 'x11'
 
-
-
+        # Starting pygame
+        pygame.init()
+        pygame.display.set_caption("Game of Life - Created by Fabio Melis")  # Gives a title to the window
+        self.screen = pygame.display.set_mode(GRID_SIZE)  # Create the window with the GRID_SIZE.
         #Clock to set the FPS
         self.clock = pygame.time.Clock()
+        # Initialise the generations
+        self.init_gen(self.current_generation, COLOR_DEAD)
+        self.root.mainloop()
 
     # Initializing all the cells.
     def init_gen(self, generation, c):
-        for y in range(self.Y_CELLS):
-            for x in range(self.X_CELLS):
-                self.generation[x][y] = c
-        # Initialise the generations
-        self.initGeneration(current_generation, COLOR_DEAD)
-
-        # Two lists, one for the current generation, and one for the next generation, so you can have iterations.
-        self.current_generation = [[COLOR_DEAD for y in range(ycells)] for x in range(xcells)]
-        self.next_generation    = [[COLOR_DEAD for y in range(ycells)] for x in range(xcells)]
-
-        self.root.mainloop()
-
+        for y in range(Y_CELLS):
+            for x in range(X_CELLS):
+                generation[x][y] = c
     # Drawing the cells, color black or blue at location x/y.
     def draw_cell(self, x, y, c):
         pos = (int(x * CELL_SIZE + CELL_SIZE / 2),
@@ -81,8 +80,8 @@ class GameOfLife:
     # Updating the cells.
     def update_gen(self):
         global current_generation
-        for y in range(self.Y_CELLS):
-            for x in range(self.X_CELLS):
+        for y in range(Y_CELLS):
+            for x in range(X_CELLS):
                 c = next_generation[x][y]
                 draw_cell(x, y, c)
         # Update current_generation
@@ -110,7 +109,6 @@ class GameOfLife:
 
     def check_cell_neighbors(self, row_index, col_index):
         # Get the number of alive cells surrounding the current cell
-        # self.grids[self.active_grid][r][c]   #is the current cell
         num_alive_neighbors = 0
         num_alive_neighbors += self.check_cells(row_index - 1, col_index - 1)
         num_alive_neighbors += self.check_cells(row_index - 1, col_index)
@@ -128,8 +126,8 @@ class GameOfLife:
     # 3 Any live cell with more than three live neighbors dies, as if by overpopulation.
     # 4 Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
     def create_next_gen(self):
-        for y in range(self.Y_CELLS):
-            for x in range(self.X_CELLS):
+        for y in range(Y_CELLS):
+            for x in range(X_CELLS):
                 # If cell is live, count neighboring live cells
                 n = check_cell_neighbors(x, y)  # number of neighbors
                 c = current_generation[x][y]  # current cell (either dead or alive).
@@ -145,12 +143,8 @@ class GameOfLife:
                         # Rule number 4: A dead cell with three living neighbors becomes alive.
                         next_generation[x][y] = COLOR_ALIVE
 
-    # Initialise variables
-
-
     # Runs the game loop
     def handle_events(self):
-        self.next_iteration = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 ended = True  # If pressing the quit button, it closes the window.
@@ -158,30 +152,29 @@ class GameOfLife:
                 posn = pygame.mouse.get_pos()
                 x = int(posn[0] / CELL_SIZE)
                 y = int(posn[1] / CELL_SIZE)
-                if next_generation[x][y] == COLOR_DEAD:
-                    activate_living_cell(x, y)
+                if self.next_generation[x][y] == COLOR_DEAD:
+                    self.activate_living_cell(x, y)
                 else:
-                    deactivate_living_cell(x, y)
+                    self.deactivate_living_cell(x, y)
             # Check for q, g, s or w keys
             if event.type == pygame.KEYDOWN:  # keydown --> quits when the button goes down. keyup --> quits when the button goes up again.
                 if event.unicode == 'q':  # Press q to quit.
-                    ended = True
+                    self.ended = True
                 elif event.key == pygame.K_SPACE:  # Space for the next iteration manually.
-                    create_next_gen()
+                    self.create_next_gen()
                 elif event.unicode == 'a':  # a to automate the iterations.
-                    next_iteration = True
+                    self.next_iteration = True
                 elif event.unicode == 's':  # s to stop the automated iterations.
-                    next_iteration = False
+                    self.next_iteration = False
                 elif event.unicode == 'r':  # r to reset the grid.
                     next_iteration = False
-                    init_gen(next_generation, COLOR_DEAD)
-
+                    self.init_gen(next_generation, COLOR_DEAD)
 
     def run(self):
-        ended = False
-        while not ended:
+
+        while not self.ended:
             self.handle_events()
-            if next_iteration:  # if next iteration is true, the next gen is created according to the rules.
+            if self.next_iteration:  # if next iteration is true, the next gen is created according to the rules.
                 create_next_gen()
 
         # Updating
